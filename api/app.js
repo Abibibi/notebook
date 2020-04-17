e78dfed0908515416c82e8fbcb2739573c9f0c49
Abibibi <abeba.ngwe@gmail.com>
2020-04-17 14:15:10 +0200

    started to bued to build backend.
    connectio to MongoDB established

---
+++ api/app.js
@@ -0,0 +1,31 @@
+// REQUIRES
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// MIDDLEWARES FOR CORS AND JSON
+app.use(cors());
+app.use(express.json());
+
+
+// DB CONNECTION
+mongoose
+  .connect(process.env.DATABASE_URI, { 
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+  })
+
+// PORT
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port: ${process.env.PORT}`);
+});
