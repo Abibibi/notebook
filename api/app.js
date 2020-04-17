16d0b33b9c20c72e07062b364038a9f483a985ca
Abibibi <abeba.ngwe@gmail.com>
2020-04-17 21:30:15 +0200

    login ok front-sidde

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ mongoose
 const connection = mongoose.connection;
 
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
+    console.log('MongoDB database connection established successfully');
 })
 
 // ROUTES
